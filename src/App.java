00832b14121675a2299ac9bc3be735e318ad2e6a
gusta <gustavobeniciosantos@gmail.com>
2023-09-18 10:41:10 -0300

    Adição do Hashmap de objeto

--- src/App.java
+++ src/App.java
@@ -5,18 +5,47 @@ public class App {
     Front front = new Front();
 
     public App(){
-    HashMap<Integer, String> disciplines = new HashMap<>();
+        write("Seja bem-vindo(a) a nossa escola!");
+        generateDiscipline();
 
     }
 
 
-    //Gero as disciplinas
-    public Discipline generateDisciplines(){
+
+
+
+    //-------------------------------------//
+    //Adiciono o Objeto num Hashmap
+    public void generateDiscipline(){
+        HashMap<Integer, Discipline> disciplinesHash = new HashMap<>();
+
+        write("Quantas disciplinas você quer na sua escola?");
+        int qtdDisciplines = read.nextInt();
+
+        for(int i = 0; i < qtdDisciplines; i++){
+
+            Discipline newDiscipline = createDiscipline();
+
+            disciplinesHash.put(newDiscipline.getDisiplineID(), newDiscipline);
+        }//forLoad
+
+        for(Map.Entry<Integer, Discipline> entry : disciplinesHash.entrySet()){
+            int id = entry.getKey();
+            String name = entry.getValue().getDisiplineName();
+
+            System.out.println("Nome: " + name + "   ID: " + id);
+        }//forPrint
+    }//generate Discipline
+
+    //----------------------------------------//
+
+    //Imputo os dados da disciplina e retorno um objeto
+    public Discipline createDiscipline(){
 
         Discipline newDiscipline = new Discipline();
 
         write("Digite o nome da disciplina");
-        newDiscipline.setDisiplineName(read.next());
+        newDiscipline.setDisiplineName(read.next());//Nome disciplina
         newDiscipline.setDisiplineID((int) (Math.random() * 90000) + 10000);//ID da disciplina
 
         return newDiscipline;
